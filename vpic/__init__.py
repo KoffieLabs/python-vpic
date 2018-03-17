6d80e3e7f553edd2551c2022db75449b605ee2d1
Damian Hites <nizebulous@gmail.com>
2018-03-17 16:59:05 -0400

    Better handling of inputs on bulk lookups.

--- vpic/__init__.py
+++ vpic/__init__.py
@@ -17,7 +17,13 @@ class VPicAPI(object):
         return Vehicle(**r.json()['Results'][0])
 
     def lookup_vins(self, vin_lookups):
-        vin_infos = [','.join(vl.values()) for vl in vin_lookups]
+        vin_infos = []
+        for vin_info in vin_lookups:
+            info = [vin_info['vin']]
+            year = vin_info.get('year')
+            if year:
+                info.append(year)
+            vin_infos.append(','.join(info))
         data = ';'.join(vin_infos)
         response = requests.post(self.url, data={'DATA': data, 'format': 'JSON'})
         response.raise_for_status()
